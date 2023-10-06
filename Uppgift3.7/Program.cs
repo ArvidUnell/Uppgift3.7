d6bd3e70349150f7e95d84c7065d0d5b67835f44
arun06001 <arun06001@E-PW0234KE>
2023-10-06 10:38:29 +0200

    Add project files.

---
+++ Uppgift3.7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en addition eller en subtraktion");
+            string problem = Console.ReadLine();
+
+            char räknesätt = problem.Contains('+') ? '+' : '-';
+
+            float tal1 = float.Parse(problem[..problem.IndexOf(räknesätt)]);
+            float tal2 = float.Parse(problem[(problem.IndexOf(räknesätt) + 1)..]);
+
+            if (räknesätt == '+')
+            {
+                Console.WriteLine($"={tal1 + tal2}");
+            }
+            else
+            {
+                Console.WriteLine($"={tal1 - tal2}");
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
